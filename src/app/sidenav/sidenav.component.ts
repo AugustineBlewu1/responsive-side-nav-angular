50b1b1444ac3a3664d3ccadd6c5877794280ecc9
Augustine Blewudzi <40745784+AugustineBlewu1@users.noreply.github.com>
2022-09-03 14:49:26 +0000

    feat: setup

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,18 @@
+import { navbarData } from './nav-data';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  collapsed = true;
+  navData = navbarData;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
